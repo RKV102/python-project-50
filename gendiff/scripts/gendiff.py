01f26df4ed7891c7e3a24f2f760b1b86057e3879
RKV102 <roman.krilow@mail.ru>
2024-01-01 03:33:19 +0500

    Небольшой рефакторинг кода

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,14 +3,18 @@ import argparse
 
 
 def main():
+    parse_arg()
+
+
+def parse_arg():
     parser = argparse.ArgumentParser(description='Compares two '
                                      + 'configuration files and '
                                      + 'shows a difference.')
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
-    parser.add_argument('-f', '--format', type=str, help='set format of output')
+    parser.add_argument('-f', '--format', type=str,
+                        help='set format of output')
     parser.parse_args()
-    # print('Test Hi')
 
 
 if __name__ == '__main__':
