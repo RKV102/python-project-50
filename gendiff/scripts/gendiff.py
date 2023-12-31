62cc3c3282ae6583c3ddc2ad440194e95203da93
RKV102 <roman.krilow@mail.ru>
2023-12-31 23:38:00 +0500

    Добавление параметра выбора формата в точку входа в приложение

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,11 +3,12 @@ import argparse
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Compares two ' +
-                                     'configuration files and ' +
-                                     'shows a difference.')
+    parser = argparse.ArgumentParser(description='Compares two '
+                                     + 'configuration files and '
+                                     + 'shows a difference.')
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
+    parser.add_argument('-f', '--format', type=str, help='set format of output')
     parser.parse_args()
     # print('Test Hi')
 
