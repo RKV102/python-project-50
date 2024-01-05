8d4f2807d8fd189388b1af77b73e5eb9f9f259d8
RKV102 <roman.krilow@mail.ru>
2024-01-06 00:34:57 +0500

    Перенос эталонной JSON-строки в новый файл новой директории

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -4,10 +4,6 @@ from gendiff import generate_diff
 def test_generate_diff():
     path_1 = './gendiff/compared/first_file.json'
     path_2 = './gendiff/compared/second_file.json'
-    str_ = '{\n  - follow: False\n'\
-           + '    host: hexlet.io\n'\
-           + '  - proxy: 123.234.53.22\n'\
-           + '  - timeout: 50\n'\
-           + '  + timeout: 20\n'\
-           + '  + verbose: True\n}'
-    assert generate_diff(path_1, path_2) == str_
+    with open('./tests/fixtures/json_verification.txt') as f:
+        json_string = f.read()
+    assert generate_diff(path_1, path_2) == json_string
