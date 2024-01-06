5ab435b67c77db9d91deaaff8a5ade59e21474ce
RKV102 <roman.krilow@mail.ru>
2024-01-06 21:59:34 +0500

    Рефакторинг JSON-парсера

--- gendiff/parsers/json_parser.py
+++ gendiff/parsers/json_parser.py
@@ -1,9 +1,7 @@
 import json
 
 
-def parse(first_file, second_file):
-    with open(first_file) as f1:
-        with open(second_file) as f2:
-            f1_content = json.load(f1)
-            f2_content = json.load(f2)
-    return [f1_content, f2_content]
+def parse(file_path):
+    with open(file_path) as file_content:
+        parsed_content = json.load(file_content)
+    return parsed_content
