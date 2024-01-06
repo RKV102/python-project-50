d7464264cb47d71eb641b932f6a63f0dc5e45ce3
RKV102 <roman.krilow@mail.ru>
2024-01-06 21:32:31 +0500

    Сравнивающиеся JSON-файлы перемещены в директорию с фикстурами

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,4 +1,4 @@
-from gendiff import generate_diff
+from gendiff.gendiff import gendiff
 
 
 def test_generate_diff():
@@ -6,4 +6,4 @@ def test_generate_diff():
     path_2 = './gendiff/compared/second_file.json'
     with open('./tests/fixtures/json_verification.txt') as f:
         json_string = f.read()
-    assert generate_diff(path_1, path_2) == json_string
+    assert gendiff(path_1, path_2) == json_string
