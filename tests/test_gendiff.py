19c2d5f556564242badb1ac37b92c56ed5d31c9b
RKV102 <roman.krilow@mail.ru>
2024-01-06 22:31:29 +0500

    Удаление старого теста и добавление нового

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,17 +1,15 @@
-from gendiff.gendiff import diff_content
+from gendiff.gendiff import diff_parsed
 from gendiff.parsers.json_parser import parse
 
 
-def test_diff_content():
-    first_file = './tests/fixtures/first_file.json'
-    with open(first_file) as f1:
-        first_content = f1.read()
-        first_parsed = parse(first_content)
-    second_file = './tests/fixtures/second_file.json'
-    with open(second_file) as f2:
-        second_content = f2.read()
-        second_parsed = parse(second_content)
-    sample_file = './tests/fixtures/json_verification.txt'
-    with open(sample_file) as sample:
+def test_diff_parsed():
+    file_path_1 = './tests/fixtures/first_file.json'
+    with open(file_path_1) as f1:
+        first_parsed = parse(file_path_1)
+    file_path_2 = './tests/fixtures/second_file.json'
+    with open(file_path_2) as f2:
+        second_parsed = parse(file_path_2)
+    file_path_3 = './tests/fixtures/json_verification.txt'
+    with open(file_path_3) as sample:
         sample_content = sample.read()
-    assert diff_content(first_content, second_content) == sample_content
+    assert diff_parsed(first_parsed, second_parsed) == sample_content
