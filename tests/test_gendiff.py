9bc94c247f49975e251fd0fb848ac8b2cb21fd00
RKV102 <roman.krilow@mail.ru>
2024-01-06 22:35:56 +0500

    Рефакторинг теста

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -4,11 +4,9 @@ from gendiff.parsers.json_parser import parse
 
 def test_diff_parsed():
     file_path_1 = './tests/fixtures/first_file.json'
-    with open(file_path_1) as f1:
-        first_parsed = parse(file_path_1)
+    first_parsed = parse(file_path_1)
     file_path_2 = './tests/fixtures/second_file.json'
-    with open(file_path_2) as f2:
-        second_parsed = parse(file_path_2)
+    second_parsed = parse(file_path_2)
     file_path_3 = './tests/fixtures/json_verification.txt'
     with open(file_path_3) as sample:
         sample_content = sample.read()
