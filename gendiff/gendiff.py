a804cc996124b51bfb203ca0e07cbd4344fe95b1
RKV102 <roman.krilow@mail.ru>
2024-01-06 21:22:03 +0500

    Рефакторинг модуля gendiff

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,9 +1,15 @@
 import argparse
+from gendiff.parsers import json_parser
 
 
 def gendiff():
-    first_file, second_file = parse_arg()
-    diff = generate_diff(first_file, second_file)
+    args = parse_arg()
+    first_file = get_item(args)
+    second_file = get_item(args)
+    parsed_content = json_parser.parse(first_file, second_file)
+    first_content = get_item(parsed_content)
+    second_content = get_item(parsed_content)
+    diff = diff_content(first_content, second_content)
     print(diff)
 
 
@@ -16,7 +22,7 @@ def parse_arg():
     parser.add_argument('-f', '--format', type=str,
                         help='set format of output')
     args = parser.parse_args()
-    return args.first_file, args.second_file
+    return [args.first_file, args.second_file]
 
 
 def diff_content(f1_content, f2_content):
@@ -63,3 +69,7 @@ def get_keys(f_content):
 
 def get_value(key, f_content):
     return f_content[key]
+
+
+def get_item(iterable):
+    return iterable.pop(0)
