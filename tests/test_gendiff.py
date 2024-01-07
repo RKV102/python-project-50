1400fc0452bccd1645983cc6d13b92a1f4e22e33
RKV102 <roman.krilow@mail.ru>
2024-01-07 18:07:26 +0500

    Переименование фикстуры

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -7,7 +7,7 @@ def test_diff_parsed():
     first_parsed = parse(file_path_1)
     file_path_2 = './tests/fixtures/second_file.json'
     second_parsed = parse(file_path_2)
-    file_path_3 = './tests/fixtures/json_verification.txt'
+    file_path_3 = './tests/fixtures/json_sample.txt'
     with open(file_path_3) as sample:
         sample_content = sample.read()
     assert diff_parsed(first_parsed, second_parsed) == sample_content
