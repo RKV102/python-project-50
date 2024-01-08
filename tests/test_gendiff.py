1461a461ec29845d5252453e714c2d82cd5e1403
RKV102 <roman.krilow@mail.ru>
2024-01-08 22:47:19 +0500

    Обновление импортов в тестах

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,12 +1,12 @@
 from gendiff.gendiff import diff_parsed
-from gendiff.parsers.json_parser import parse
+from gendiff import parsers
 
 
 def test_diff_parsed():
     file_path_1 = './tests/fixtures/first_file.json'
-    first_parsed = parse(file_path_1)
+    first_parsed = parsers.parse_json(file_path_1)
     file_path_2 = './tests/fixtures/second_file.json'
-    second_parsed = parse(file_path_2)
+    second_parsed = parsers.parse_json(file_path_2)
     file_path_3 = './tests/fixtures/json_sample.txt'
     with open(file_path_3) as sample:
         sample_content = sample.read()
