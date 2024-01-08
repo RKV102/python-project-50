d36df2f2cecb8ef2a46a293c0d46c0853c144688
RKV102 <roman.krilow@mail.ru>
2024-01-08 21:31:57 +0500

    Удаление get_keys() и get_value()

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -9,18 +9,18 @@ def gendiff(file_path_1, file_path_2):
 
 
 def diff_parsed(parsed_content_1, parsed_content_2):
-    first_keys = get_keys(parsed_content_1)
-    second_keys = get_keys(parsed_content_2)
+    first_keys = parsed_content_1.keys()
+    second_keys = parsed_content_2.keys()
     united_keys = unite_keys(first_keys, second_keys)
     united_keys.sort()
     diff = '{\n'
     for key in united_keys:
         match key in first_keys:
             case True:
-                first_value = get_value(key, parsed_content_1)
+                first_value = parsed_content_1[key]
                 match key in second_keys:
                     case True:
-                        second_value = get_value(key, parsed_content_2)
+                        second_value = parsed_content_2[key]
                         match first_value == second_value:
                             case True:
                                 diff = f'{diff}    {key}: {first_value}\n'
@@ -30,7 +30,7 @@ def diff_parsed(parsed_content_1, parsed_content_2):
                     case _:
                         diff = f'{diff}  - {key}: {first_value}\n'
             case _:
-                second_value = get_value(key, parsed_content_2)
+                second_value = parsed_content_2[key]
                 diff = f'{diff}  + {key}: {second_value}\n'
     diff += '}'
     return diff
@@ -42,11 +42,3 @@ def unite_keys(first_keys, second_keys):
     union_set = first_set.union(second_set)
     united_keys = list(union_set)
     return united_keys
-
-
-def get_keys(parsed_content):
-    return list(parsed_content.keys())
-
-
-def get_value(key, parsed_content):
-    return parsed_content[key]
