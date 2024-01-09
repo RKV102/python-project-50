85823e4b2c1f3bf25efb77c1fd4372df4bcbd5c8
RKV102 <roman.krilow@mail.ru>
2024-01-09 16:43:52 +0500

    Обновление тестов

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,25 +1,23 @@
-from gendiff.gendiff import diff_parsed
-from gendiff.parsers import json_parser, yaml_parser
+from gendiff.gendiff import gendiff
 
 
 def test_diff_parsed():
 
     # JSON-тестирование
     file_path_1 = './tests/fixtures/first_file.json'
-    first_parsed = json_parser.parse(file_path_1)
     file_path_2 = './tests/fixtures/second_file.json'
-    second_parsed = json_parser.parse(file_path_2)
     file_path_3 = './tests/fixtures/json_sample.txt'
     with open(file_path_3) as sample:
         sample_content = sample.read()
-    assert diff_parsed(first_parsed, second_parsed) == sample_content
+    assert gendiff(file_path_1, file_path_2) == sample_content
 
     # YML/YAML-тестирование
     file_path_1 = './tests/fixtures/first_file.yaml'
-    first_parsed = yaml_parser.parse(file_path_1)
     file_path_2 = './tests/fixtures/second_file.yaml'
-    second_parsed = yaml_parser.parse(file_path_2)
     file_path_3 = './tests/fixtures/yaml_sample.txt'
     with open(file_path_3) as sample:
         sample_content = sample.read()
-    assert diff_parsed(first_parsed, second_parsed) == sample_content
+    assert gendiff(file_path_1, file_path_2) == sample_content
+
+    # YAML- и TXT-тестирование
+    assert gendiff(file_path_1, file_path_3) is None
