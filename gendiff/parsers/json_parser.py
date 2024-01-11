99815590ea9ef633ecf6fa96639c7f2cef0bab80
RKV102 <roman.krilow@mail.ru>
2024-01-11 16:45:15 +0500

    Удаление директории с парсерами

--- gendiff/parsers/json_parser.py
+++
@@ -1,7 +0,0 @@
-import json
-
-
-def parse(file_path):
-    with open(file_path) as file_content:
-        parsed_content = json.load(file_content)
-    return parsed_content
--- gendiff/parsers/yaml_parser.py
+++
@@ -1,7 +0,0 @@
-import yaml
-
-
-def parse(file_path):
-    with open(file_path) as file_content:
-        parsed_content = yaml.load(file_content, Loader=yaml.Loader)
-    return parsed_content
