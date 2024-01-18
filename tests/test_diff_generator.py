03f7ddeccbd921165dcff7bc86e4eed895eb8648
RKV102 <roman.krilow@mail.ru>
2024-01-18 21:48:28 +0500

    Правка тестов

--- tests/test_diff_generator.py
+++ tests/test_diff_generator.py
@@ -6,18 +6,18 @@ def test_generate_diff():
     # JSON-тестирование
     file_path_1 = './tests/fixtures/first_file.json'
     file_path_2 = './tests/fixtures/second_file.json'
-    file_path_3 = './tests/fixtures/json_sample.txt'
+    file_path_3 = './tests/fixtures/json_stylish_sample.txt'
     with open(file_path_3) as sample:
         sample_content = sample.read()
     assert str(generate_diff('_', file_path_1, file_path_2)) == sample_content
 
     # YML/YAML-тестирование
-    # file_path_1 = './tests/fixtures/first_file.yaml'
-    # file_path_2 = './tests/fixtures/second_file.yaml'
-    # file_path_3 = './tests/fixtures/yaml_sample.txt'
-    # with open(file_path_3) as sample:
-    #     sample_content = sample.read()
-    # assert str(generate_diff('_', file_path_1, file_path_2)) == sample_content
+    file_path_1 = './tests/fixtures/first_file.yaml'
+    file_path_2 = './tests/fixtures/second_file.yaml'
+    file_path_3 = './tests/fixtures/yaml_stylish_sample.txt'
+    with open(file_path_3) as sample:
+        sample_content = sample.read()
+    assert str(generate_diff('_', file_path_1, file_path_2)) == sample_content
 
     # YAML- и TXT-тестирование
     assert generate_diff('_', file_path_1, file_path_3) is None
