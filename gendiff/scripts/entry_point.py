5e23e2ec9a6d0b82ee8188acadc3cf49b344760b
RKV102 <roman.krilow@mail.ru>
2024-01-18 20:01:22 +0500

    Теперь параметр формата имеет значение по умолчанию

--- gendiff/scripts/entry_point.py
+++ gendiff/scripts/entry_point.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import argparse
+import gendiff.formatters as formatters
 from gendiff.diff_generator import generate_diff
-from gendiff.formatters.stylish import format
 
 
 def main():
@@ -11,12 +11,15 @@ def main():
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
     parser.add_argument('-f', '--format', type=str,
+                        default='stylish', dest='format',
                         help='set format of output.')
     args = parser.parse_args()
     file_path_1 = args.first_file
     file_path_2 = args.second_file
+    # format = args.format
     diff = generate_diff(file_path_1, file_path_2)
-    print(format(diff))
+    formatted_diff = formatters.stylish.format(diff)
+    print(formatted_diff)
 
 
 if __name__ == '__main__':
