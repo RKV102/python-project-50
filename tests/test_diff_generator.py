d8cadf0162586b8d9a1aaf25bc21b6c90a420e4d
RKV102 <roman.krilow@mail.ru>
2024-01-23 20:28:10 +0500

    Переименование некоторых фикстур и переменных

--- tests/test_diff_generator.py
+++ tests/test_diff_generator.py
@@ -4,19 +4,19 @@ import yaml
 from gendiff.diff_generator import diff_parsed
 from tests.fixtures.json_diff import diff as json_diff
 from tests.fixtures.yaml_diff import diff as yaml_diff
-with open('./tests/fixtures/first_input.json') as first_input_json:
-    first_input_json_content = json.load(first_input_json)
-with open('./tests/fixtures/second_input.json') as second_input_json:
-    second_input_json_content = json.load(second_input_json)
-with open('./tests/fixtures/first_input.yml') as first_input_yml:
-    first_input_yml_content = yaml.load(first_input_yml, Loader=yaml.Loader)
-with open('./tests/fixtures/second_input.yml') as second_input_yml:
-    second_input_yml_content = yaml.load(second_input_yml, Loader=yaml.Loader)
+with open('./tests/fixtures/first.json') as first_json:
+    first_parsed_json = json.load(first_json)
+with open('./tests/fixtures/second.json') as second_json:
+    second_parsed_json = json.load(second_json)
+with open('./tests/fixtures/first.yml') as first_yml:
+    first_parsed_yml = yaml.load(first_yml, Loader=yaml.Loader)
+with open('./tests/fixtures/second.yml') as second_yml:
+    second_parsed_yml = yaml.load(second_yml, Loader=yaml.Loader)
 
 
 @pytest.mark.parametrize('input, expected', [
-    ((first_input_json_content, second_input_json_content), json_diff),
-    ((first_input_yml_content, second_input_yml_content), yaml_diff)
+    ((first_parsed_json, second_parsed_json), json_diff),
+    ((first_parsed_yml, second_parsed_yml), yaml_diff)
 ])
 def test_diff_parsed(input, expected):
     assert diff_parsed(*input) == expected
