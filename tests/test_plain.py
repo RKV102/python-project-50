921224f630f2f27d174f95b8f8c176efcbb7c36b
RKV102 <roman.krilow@mail.ru>
2024-01-23 21:19:51 +0500

    Небольшой рефакторинг тестов

--- tests/test_plain.py
+++ tests/test_plain.py
@@ -1,18 +1,18 @@
 import pytest
 from tests.fixtures.json_diff import diff as json_diff
 from tests.fixtures.yaml_diff import diff as yaml_diff
+from tests.test_stylish import FILE_PATH_START, FILE_EXTENSIONS
 from gendiff.formatters.plain import format
-JSON_SAMPLE_PATH = './tests/fixtures/plain_json.txt'
-YAML_SAMPLE_PATH = './tests/fixtures/plain_yaml.txt'
-with open(JSON_SAMPLE_PATH) as json_sample:
-    json_sample_content = json_sample.read()
-with open(YAML_SAMPLE_PATH) as yaml_sample:
-    yaml_sample_content = yaml_sample.read()
+samples = {}
+for i in ('json', 'yaml'):
+    with open(f'{FILE_PATH_START}plain_{i}{FILE_EXTENSIONS}') \
+            as sample:
+        samples[i] = sample.read()
 
 
 @pytest.mark.parametrize('input, expected', [
-    (json_diff, json_sample_content),
-    (yaml_diff, yaml_sample_content)
+    (json_diff, samples['json']),
+    (yaml_diff, samples['yaml'])
 ])
 def test_format(input, expected):
     assert format(input) == expected
--- tests/test_stylish.py
+++ tests/test_stylish.py
@@ -2,17 +2,18 @@ import pytest
 from tests.fixtures.json_diff import diff as json_diff
 from tests.fixtures.yaml_diff import diff as yaml_diff
 from gendiff.formatters.stylish import format
-JSON_SAMPLE_PATH = './tests/fixtures/stylish_json.txt'
-YAML_SAMPLE_PATH = './tests/fixtures/stylish_yaml.txt'
-with open(JSON_SAMPLE_PATH) as json_sample:
-    json_sample_content = json_sample.read()
-with open(YAML_SAMPLE_PATH) as yaml_sample:
-    yaml_sample_content = yaml_sample.read()
+FILE_PATH_START = './tests/fixtures/'
+FILE_EXTENSIONS = '.txt'
+samples = {}
+for i in ('json', 'yaml'):
+    with open(f'{FILE_PATH_START}stylish_{i}{FILE_EXTENSIONS}') \
+            as sample:
+        samples[i] = sample.read()
 
 
 @pytest.mark.parametrize('input, expected', [
-    (json_diff, json_sample_content),
-    (yaml_diff, yaml_sample_content)
+    (json_diff, samples['json']),
+    (yaml_diff, samples['yaml'])
 ])
 def test_format(input, expected):
     assert format(input) == expected
