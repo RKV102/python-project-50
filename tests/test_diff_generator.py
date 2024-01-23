a6e2b2d33441b61ad76a7692e8fbba002eee6133
RKV102 <roman.krilow@mail.ru>
2024-01-23 18:43:30 +0500

    Сокращение дублирующего кода в тестах

--- tests/test_diff_generator.py
+++ tests/test_diff_generator.py
@@ -1,3 +1,4 @@
+import pytest
 from gendiff.diff_generator import diff_parsed
 from tests.fixtures.first_parsed_json import parsed as json_1
 from tests.fixtures.second_parsed_json import parsed as json_2
@@ -7,10 +8,9 @@ from tests.fixtures.json_diff import diff as json_diff
 from tests.fixtures.yaml_diff import diff as yaml_diff
 
 
-def test_diff_parsed():
-
-    # JSON-тестирование
-    assert diff_parsed(json_1, json_2) == json_diff
-
-    # YAML-тестирование
-    assert diff_parsed(yaml_1, yaml_2) == yaml_diff
+@pytest.mark.parametrize('input, expected', [
+    ((json_1, json_2), json_diff),
+    ((yaml_1, yaml_2), yaml_diff)
+])
+def test_diff_parsed(input, expected):
+    assert diff_parsed(*input) == expected
--- tests/test_stylish.py
+++ tests/test_stylish.py
@@ -1,18 +1,18 @@
+import pytest
 from tests.fixtures.json_diff import diff as json_diff
 from tests.fixtures.yaml_diff import diff as yaml_diff
 from gendiff.formatters.stylish import format
 JSON_SAMPLE_PATH = './tests/fixtures/stylish_json.txt'
 YAML_SAMPLE_PATH = './tests/fixtures/stylish_yaml.txt'
+with open(JSON_SAMPLE_PATH) as json_sample:
+    json_sample_content = json_sample.read()
+with open(YAML_SAMPLE_PATH) as yaml_sample:
+    yaml_sample_content = yaml_sample.read()
 
 
-def test_format():
-
-    # JSON-тестирование
-    with open(JSON_SAMPLE_PATH) as json_sample:
-        json_sample_content = json_sample.read()
-    assert format(json_diff) == json_sample_content
-
-    # YAML-тестирование
-    with open(YAML_SAMPLE_PATH) as yaml_sample:
-        yaml_sample_content = yaml_sample.read()
-    assert format(yaml_diff) == yaml_sample_content
+@pytest.mark.parametrize('input, expected', [
+    (json_diff, json_sample_content),
+    (yaml_diff, yaml_sample_content)
+])
+def test_format(input, expected):
+    assert format(input) == expected
