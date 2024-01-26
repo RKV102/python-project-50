cd38b745cc9cfb2e4c3d24c29beb643067bba598
RKV102 <roman.krilow@mail.ru>
2024-01-26 22:19:59 +0500

    Удаление f-строки

--- tests/test_parsers_runner.py
+++ tests/test_parsers_runner.py
@@ -2,4 +2,4 @@ from gendiff.parsers_runner import run_parser
 
 
 def test_run_parser():
-    assert run_parser(f'./tests/fixtures/unknown.png', '_') is None
+    assert run_parser('./tests/fixtures/unknown.png', '_') is None
