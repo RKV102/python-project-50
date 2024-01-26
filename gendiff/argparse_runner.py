ab07c6ca840ae5821305557721dd1743c04c5539
RKV102 <roman.krilow@mail.ru>
2024-01-26 22:17:40 +0500

    Перенос логики argparse в отдельный модуль

---
+++ gendiff/argparse_runner.py
@@ -0,0 +1,14 @@
+import argparse
+
+
+def run_argparse():
+    parser = argparse.ArgumentParser(description='Compares two '
+                                                 + 'configuration files and '
+                                                 + 'shows a difference.')
+    parser.add_argument('first_file', type=str)
+    parser.add_argument('second_file', type=str)
+    parser.add_argument('-f', '--format', type=str,
+                        default='stylish', dest='format',
+                        help='set format of output.')
+    args = parser.parse_args()
+    return args.first_file, args.second_file, args.format
--- gendiff/scripts/entry_point.py
+++ gendiff/scripts/entry_point.py
@@ -1,21 +1,10 @@
 #!/usr/bin/env python3
-import argparse
+from gendiff.argparse_runner import run_argparse
 from gendiff.diff_generator import generate_diff
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Compares two '
-                                     + 'configuration files and '
-                                     + 'shows a difference.')
-    parser.add_argument('first_file', type=str)
-    parser.add_argument('second_file', type=str)
-    parser.add_argument('-f', '--format', type=str,
-                        default='stylish', dest='formatter',
-                        help='set format of output.')
-    args = parser.parse_args()
-    file_path_1 = args.first_file
-    file_path_2 = args.second_file
-    formatter = args.formatter
+    file_path_1, file_path_2, formatter = run_argparse()
     generate_diff(file_path_1, file_path_2, formatter=formatter)
 
 
