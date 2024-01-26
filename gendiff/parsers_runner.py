efa7054b4fe87eae923a0b7101ae65f85b7457cf
RKV102 <roman.krilow@mail.ru>
2024-01-26 22:05:02 +0500

    Добавлена обработка исключения для модуля parsers_runner

--- gendiff/parsers_runner.py
+++ gendiff/parsers_runner.py
@@ -1,5 +1,8 @@
 def run_parser(file_path, parser, loader=None):
-    with open(file_path) as file_content:
-        parsed_content = parser.load(file_content) if not loader\
-            else parser.load(file_content, Loader=loader)
-    return parsed_content
+    try:
+        with open(file_path) as file_content:
+            parsed_content = parser.load(file_content) if not loader\
+                else parser.load(file_content, Loader=loader)
+        return parsed_content
+    except FileNotFoundError:
+        print(f'No such file or directory. See: {file_path}')
