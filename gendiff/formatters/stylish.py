a8c6a38e001b5748bd5a6d4ce94d68a454ebd288
RKV102 <roman.krilow@mail.ru>
2024-01-31 20:16:50 +0500

    Теперь format() больше не имеет inner()

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -7,49 +7,45 @@ PAIRS_OF_VALUES = {
 }
 
 
-def format(diff):
-
-    def inner(diff, level=1):
-        indent = INDENT_SYMBOL * INDENT_LEN * level
-        message = ''
-        for item in diff.items():
-            key = item[0]
-            value = item[1]['nested']
-            value_type = str(type(value))[8:-2]
-            action = item[1].get('action')
-            message_start = f'{indent[:-2]}'
-            match value_type:
-                case 'dict':
-                    message_end = create_message_end(key, value,
-                                                     indent=indent,
-                                                     level=level,
-                                                     inner=inner)
+def format(diff, level=1):
+    indent = INDENT_SYMBOL * INDENT_LEN * level
+    message = ''
+    for item in diff.items():
+        key = item[0]
+        value = item[1]['nested']
+        value_type = str(type(value))[8:-2]
+        action = item[1].get('action')
+        message_start = f'{indent[:-2]}'
+        match value_type:
+            case 'dict':
+                message_end = create_message_end(key, value,
+                                                 indent=indent,
+                                                 level=level,
+                                                 format=format)
+                message += create_message(message_start, message_end,
+                                          action=action, indent=indent)
+            case 'list':
+                for i in ((value[0], '-'), (value[1], '+')):
+                    if isinstance(i[0], dict):
+                        message_end = create_message_end(key, i[0],
+                                                         indent=indent,
+                                                         level=level,
+                                                         format=format)
+                    else:
+                        message_end = create_message_end(key, i[0])
                     message += create_message(message_start, message_end,
-                                              action=action, indent=indent)
-                case 'list':
-                    for i in ((value[0], '-'), (value[1], '+')):
-                        if isinstance(i[0], dict):
-                            message_end = create_message_end(key, i[0],
-                                                             indent=indent,
-                                                             level=level,
-                                                             inner=inner)
-                        else:
-                            message_end = create_message_end(key, i[0])
-                        message += create_message(message_start, message_end,
-                                                  sign=i[1])
-                case _:
-                    message_end = create_message_end(key, value)
-                    message += create_message(message_start, message_end,
-                                              action=action, indent=indent)
-        return message
-
-    return '{\n' + inner(diff) + '}'
+                                              sign=i[1])
+            case _:
+                message_end = create_message_end(key, value)
+                message += create_message(message_start, message_end,
+                                          action=action, indent=indent)
+    return '{\n' + message + '}' if level == 1 else message
 
 
 def create_message_end(key, value, **kwargs):
     if not kwargs.get('indent'):
         return f'{key}: {transform(value)}\n'
-    return (f'{key}:' + ' {\n' + kwargs['inner'](value, kwargs['level'] + 1)
+    return (f'{key}:' + ' {\n' + kwargs['format'](value, kwargs['level'] + 1)
             + kwargs['indent'] + '}\n')
 
 
