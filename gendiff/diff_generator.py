0cd881c23404d8255195ebde7a0ea50036eb9ece
RKV102 <roman.krilow@mail.ru>
2024-02-03 00:58:35 +0500

    Рефакторинг diff_parsed()

--- gendiff/diff_generator.py
+++ gendiff/diff_generator.py
@@ -10,7 +10,7 @@ def generate_diff(file_path1, file_path2, formatter='stylish'):
 
 
 def diff_parsed(parsed1, parsed2={}, was_action=False):
-    if not isinstance(parsed1, dict):
+    if not isinstance(parsed1, dict) and not parsed2:
         return parsed1
     view = {}
     keys1 = set(parsed1.keys())
@@ -20,42 +20,23 @@ def diff_parsed(parsed1, parsed2={}, was_action=False):
     united_keys = list(keys1.union(keys2))
     united_keys.sort()
     for key in united_keys:
-        if key_in_added_or_removed_keys(key, removed_keys, added_keys,
-                                        was_action=was_action, view=view,
-                                        parsed1=parsed1, parsed2=parsed2):
-            continue
+        if key in removed_keys:
+            add_to_view(view, key, parsed1, 'removed', was_action)
+        elif key in added_keys:
+            add_to_view(view, key, parsed2, 'added', was_action)
         elif parsed1[key] == parsed2[key]:
-            view[key] = {'nested': diff_parsed(parsed1[key], parsed2[key],
-                                               was_action=True)}
-            check_action(was_action, 'same', view[key])
-        elif (not isinstance(parsed1[key], dict)
-              or not isinstance(parsed2[key], dict)):
-            view[key] = {'nested': [diff_parsed(parsed1[key], was_action=True),
-                                    diff_parsed(parsed2[key], was_action=True)]}
-            check_action(was_action, 'updated', view[key])
-        else:
+            add_to_view(view, key, parsed1, 'same', was_action)
+        elif isinstance(parsed1[key], dict) and isinstance(parsed2[key], dict):
             view[key] = {'nested': diff_parsed(parsed1[key], parsed2[key])}
-
+        else:
+            view[key] = {'nested': [diff_parsed(parsed1[key], was_action=True),
+                                    diff_parsed(parsed2[key], was_action=True)],
+                         'action': 'updated'}
     return view
 
 
-def check_action(was_action, action, key_of_view):
-    if not was_action:
-        key_of_view['action'] = action
-
-
-def key_in_added_or_removed_keys(key, removed_keys, added_keys, **kwargs):
-    action = None
-    parsed = None
-    if key in removed_keys:
-        parsed = kwargs['parsed1']
-        action = 'removed'
-    elif key in added_keys:
-        parsed = kwargs['parsed2']
-        action = 'added'
-    if action:
-        kwargs['view'][key] = {'nested': diff_parsed(parsed[key],
-                                                     was_action=True)}
-        check_action(kwargs['was_action'], action, kwargs['view'][key])
-        return True
-    return False
+def add_to_view(view, key, source, action, was_action):
+    view[key] = {'nested': diff_parsed(source[key], was_action=True),
+                 'action': action} \
+        if not was_action \
+        else {'nested': diff_parsed(source[key], was_action=True)}
