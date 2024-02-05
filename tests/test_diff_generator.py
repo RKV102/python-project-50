2e62716247bef01d5a8e8fa297ff8c0ae50eb792
RKV102 <roman.krilow@mail.ru>
2024-02-05 19:44:41 +0500

    Рефакторинг тестов

--- tests/test_diff_generator.py
+++ tests/test_diff_generator.py
@@ -3,15 +3,12 @@ from gendiff.diff_generator import generate_diff
 from tests.fixture_handler import get_fixture_path, load_fixture_content
 
 
-@pytest.mark.parametrize('input1, input2, formatter, expected', [
-    (get_fixture_path('first.json'), get_fixture_path('second.json'), 'stylish',
-     load_fixture_content('stylish_json.txt')),
-    (get_fixture_path('first.yml'), get_fixture_path('second.yml'), 'stylish',
-     load_fixture_content('stylish_yml.txt')),
-    (get_fixture_path('first.json'), get_fixture_path('second.json'), 'plain',
-     load_fixture_content('plain_json.txt')),
-    (get_fixture_path('first.yml'), get_fixture_path('second.yml'), 'plain',
-     load_fixture_content('plain_yml.txt'))
+@pytest.mark.parametrize('input1, input2, formatter, output', [
+    ('first.json', 'second.json', 'stylish', 'stylish_json.txt'),
+    ('first.yml', 'second.yml', 'stylish', 'stylish_yml.txt'),
+    ('first.json', 'second.json', 'plain', 'plain_json.txt'),
+    ('first.yml', 'second.yml', 'plain', 'plain_yml.txt')
 ])
-def test_generate_diff(input1, input2, formatter, expected):
-    assert generate_diff(input1, input2, formatter) == expected
+def test_generate_diff(input1, input2, formatter, output):
+    assert generate_diff(get_fixture_path(input1), get_fixture_path(input2),
+                         formatter) == load_fixture_content(output)
