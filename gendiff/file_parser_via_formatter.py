b978022d0bc04e9cdacadcc8c5c6177f96e207ce
RKV102 <roman.krilow@mail.ru>
2024-02-05 19:45:15 +0500

    Рефакторинг parse_file()

--- gendiff/file_parser_via_formatter.py
+++ gendiff/file_parser_via_formatter.py
@@ -5,19 +5,14 @@ from os.path import splitext
 
 def parse_file(file_path):
     _, file_extension = splitext(file_path)
-    match file_extension:
-        case '.json':
-            return run_parser(file_path, json)
-        case '.yaml' | '.yml':
-            return run_parser(file_path, yaml, yaml.Loader)
-        case _:
-            raise ValueError(f'Unsupported file type. See: {file_path}')
-
-
-def run_parser(file_path, parser, parser_loader=None):
     try:
         with open(file_path) as opened_file:
-            return (parser.load(opened_file) if not parser_loader
-                    else parser.load(opened_file, Loader=parser_loader))
+            match file_extension:
+                case '.json':
+                    return json.load(opened_file)
+                case '.yaml' | '.yml':
+                    return yaml.load(opened_file, Loader=yaml.Loader)
+                case _:
+                    raise ValueError(f'Unsupported file type. See: {file_path}')
     except FileNotFoundError:
         raise FileNotFoundError(f'No such file or directory. See: {file_path}')
