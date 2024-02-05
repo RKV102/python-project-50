81efe74ffca1f633d2d8eb46ae1f3985a0856126
RKV102 <roman.krilow@mail.ru>
2024-02-05 20:17:27 +0500

    Правка объявления пути к фикстуре

--- tests/fixture_handler.py
+++ tests/fixture_handler.py
@@ -1,8 +1,11 @@
-FIXTURE_PATH_START = './tests/fixtures/'
+from os.path import join
+
+
+FIXTURE_PATH_START = join('.', 'tests', 'fixtures')
 
 
 def get_fixture_path(fixture_name):
-    return f'{FIXTURE_PATH_START}{fixture_name}'
+    return join(FIXTURE_PATH_START, fixture_name)
 
 
 def load_fixture_content(fixture_name):
