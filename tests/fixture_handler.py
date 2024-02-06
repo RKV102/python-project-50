63b03960e87163239bee41ce5e5c05b6ae3bb463
RKV102 <roman.krilow@mail.ru>
2024-02-07 00:18:39 +0500

    Переделано создание значения FIXTURE_PATH_START

--- tests/fixture_handler.py
+++ tests/fixture_handler.py
@@ -1,7 +1,7 @@
-from os.path import join
+from os.path import dirname, abspath, join
 
 
-FIXTURE_PATH_START = join('.', 'tests', 'fixtures')
+FIXTURE_PATH_START = join(dirname(abspath(__file__)), 'fixtures')
 
 
 def get_fixture_path(fixture_name):
