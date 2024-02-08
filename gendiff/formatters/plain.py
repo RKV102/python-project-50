2cd782654cf3a779a924f8ab09f7816d6c71eb7e
RKV102 <roman.krilow@mail.ru>
2024-02-08 17:46:30 +0500

    Вынесение inner() наружу под новым именем

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -6,39 +6,39 @@ COMPLEX_VALUE = '[complex value]'
 
 
 def format(diff):
+    return format_inner(diff)[:-1]
 
-    def inner(diff, input_dir=[]):
-        message = []
-        for item in diff.items():
-            key = item[0]
-            value = item[1]['nested']
-            action = item[1].get('action')
-            dir = [*input_dir, key]
-            transformed_dir = transform('.'.join(dir), True)
-            match action:
-                case 'removed':
-                    message.append(MESSAGE_START + transformed_dir
-                                   + ' was removed\n')
-                case 'added':
-                    message.append(MESSAGE_START + transformed_dir
-                                   + ' was added with value: '
-                                   + (COMPLEX_VALUE if isinstance(value, dict)
-                                      else transform(value, True))
-                                   + '\n')
-                case 'updated':
-                    message.append(MESSAGE_START + transformed_dir
-                                   + ' was updated. From '
-                                   + ' to '.join(
-                                     [COMPLEX_VALUE
-                                      if isinstance(sub_value, dict)
-                                      else transform(sub_value, True)
-                                      for sub_value in value]
-                                   )
-                                   + '\n')
-                case 'same':
-                    pass
-                case _:
-                    message.append(inner(value, dir))
-        return ''.join(message)
 
-    return inner(diff)[:-1]
+def format_inner(diff, input_dir=[]):
+    message = []
+    for item in diff.items():
+        key = item[0]
+        value = item[1]['nested']
+        action = item[1].get('action')
+        dir = [*input_dir, key]
+        transformed_dir = transform('.'.join(dir), True)
+        match action:
+            case 'removed':
+                message.append(MESSAGE_START + transformed_dir
+                               + ' was removed\n')
+            case 'added':
+                message.append(MESSAGE_START + transformed_dir
+                               + ' was added with value: '
+                               + (COMPLEX_VALUE if isinstance(value, dict)
+                                  else transform(value, True))
+                               + '\n')
+            case 'updated':
+                message.append(MESSAGE_START + transformed_dir
+                               + ' was updated. From '
+                               + ' to '.join(
+                                 [COMPLEX_VALUE
+                                  if isinstance(sub_value, dict)
+                                  else transform(sub_value, True)
+                                  for sub_value in value]
+                               )
+                               + '\n')
+            case 'same':
+                pass
+            case _:
+                message.append(format_inner(value, dir))
+    return ''.join(message)
