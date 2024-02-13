35972bb63fa0b96ee2f73c76b5b3a342dbb56493
RKV102 <roman.krilow@mail.ru>
2024-02-13 21:48:30 +0500

    Теперь переменная opened_file называется file_content

--- gendiff/file_parser_via_formatter.py
+++ gendiff/file_parser_via_formatter.py
@@ -5,15 +5,15 @@ from os.path import splitext
 
 def parse_file(file_path):
     _, file_extension = splitext(file_path)
-    with open(file_path) as opened_file:
-        return load_to_parser(opened_file, file_extension, file_path)
+    with open(file_path) as file_content:
+        return load_to_parser(file_content, file_extension, file_path)
 
 
-def load_to_parser(opened_file, file_extension, file_path):
+def load_to_parser(file_content, file_extension, file_path):
     match file_extension:
         case '.json':
-            return json.load(opened_file)
+            return json.load(file_content)
         case '.yaml' | '.yml':
-            return yaml.load(opened_file, Loader=yaml.Loader)
+            return yaml.load(file_content, Loader=yaml.Loader)
         case _:
             raise ValueError(f'Unsupported file type. See: {file_path}')
