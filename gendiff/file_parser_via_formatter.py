12b1535fe4f627286147fbde583531aca2c681a3
RKV102 <roman.krilow@mail.ru>
2024-02-13 21:53:09 +0500

    file_path больше не является аргументом load_to_parser()

--- gendiff/file_parser_via_formatter.py
+++ gendiff/file_parser_via_formatter.py
@@ -6,14 +6,14 @@ from os.path import splitext
 def parse_file(file_path):
     _, file_extension = splitext(file_path)
     with open(file_path) as file_content:
-        return load_to_parser(file_content, file_extension, file_path)
+        return load_to_parser(file_content, file_extension)
 
 
-def load_to_parser(file_content, file_extension, file_path):
+def load_to_parser(file_content, file_extension):
     match file_extension[1:]:
         case 'json':
             return json.load(file_content)
         case 'yaml' | 'yml':
             return yaml.load(file_content, Loader=yaml.Loader)
         case _:
-            raise ValueError(f'Unsupported file type. See: {file_path}')
+            raise ValueError(f'Unsupported file type.')
