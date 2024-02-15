0e8f1e9279cc4d12c3c479d65e0df63b2680ee87
RKV102 <roman.krilow@mail.ru>
2024-02-15 19:27:02 +0500

    Теперь список messages называется lines

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -7,33 +7,33 @@ def format(diff):
 
 
 def format_inner(diff, input_dir=()):
-    messages = []
+    lines = []
     for key, (value, status) in diff.items():
         dir = (*input_dir, key)
         dir_with_quotes = make_quotes('.'.join(dir))
         match status:
             case 'removed':
-                messages.append(MESSAGE_START + dir_with_quotes
-                                + ' was removed\n')
+                lines.append(MESSAGE_START + dir_with_quotes
+                             + ' was removed\n')
             case 'added':
-                messages.append(MESSAGE_START + dir_with_quotes
-                                + ' was added with value: '
-                                + (COMPLEX_VALUE if isinstance(value, dict)
-                                   else make_quotes(value))
-                                + '\n')
+                lines.append(MESSAGE_START + dir_with_quotes
+                             + ' was added with value: '
+                             + (COMPLEX_VALUE if isinstance(value, dict)
+                                else make_quotes(value))
+                             + '\n')
             case 'nested':
-                messages.append(format_inner(value, dir))
+                lines.append(format_inner(value, dir))
             case 'updated':
-                messages.append(MESSAGE_START + dir_with_quotes
-                                + ' was updated. From '
-                                + ' to '.join(
-                                  [COMPLEX_VALUE
-                                   if isinstance(sub_value, dict)
-                                   else make_quotes(sub_value)
-                                   for sub_value in value]
-                                )
-                                + '\n')
-    return ''.join(messages)
+                lines.append(MESSAGE_START + dir_with_quotes
+                             + ' was updated. From '
+                             + ' to '.join(
+                               [COMPLEX_VALUE
+                                if isinstance(sub_value, dict)
+                                else make_quotes(sub_value)
+                                for sub_value in value]
+                             )
+                             + '\n')
+    return ''.join(lines)
 
 
 def make_quotes(value):
--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -9,27 +9,27 @@ def format(diff):
 def format_inner(diff, level=1, was_status=False):
     indent = INDENT_SYMBOL * INDENT_LEN * level
     indent_before_the_sign = f'{indent[:-2]}'
-    messages = []
+    lines = []
     for key, value_and_status in diff.items():
         (value, status) = (value_and_status[0], value_and_status[1]) \
             if not was_status else (value_and_status, None)
         match status:
             case 'removed':
-                messages.append(f'{indent_before_the_sign}- {key}: '
-                                + transform(value, level, indent, status))
+                lines.append(f'{indent_before_the_sign}- {key}: '
+                             + transform(value, level, indent, status))
             case 'added':
-                messages.append(f'{indent_before_the_sign}+ {key}: '
-                                + transform(value, level, indent, status))
+                lines.append(f'{indent_before_the_sign}+ {key}: '
+                             + transform(value, level, indent, status))
             case 'updated':
                 appended = []
                 for value_, sign in ((value[0], '-'), (value[1], '+')):
                     appended.append(f'{indent_before_the_sign}{sign} {key}: '
                                     + transform(value_, level, indent, status))
-                messages.append(''.join(appended))
+                lines.append(''.join(appended))
             case _:
-                messages.append(f'{indent}{key}: '
-                                + transform(value, level, indent, status))
-    return ''.join(messages)
+                lines.append(f'{indent}{key}: '
+                             + transform(value, level, indent, status))
+    return ''.join(lines)
 
 
 def transform(value, level, indent, status):
