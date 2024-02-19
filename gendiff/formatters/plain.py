dfebb61eab90164b3602e6cd139d6957633d0182
RKV102 <roman.krilow@mail.ru>
2024-02-19 16:54:09 +0500

    Рефакторинг plain

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -1,46 +1,38 @@
-MESSAGE_START = 'Property '
+LINE_START = 'Property '
 
 
-def format(diff):
-    return format_inner(diff)[:-1]
-
-
-def format_inner(diff, input_dir=''):
+def format(diff, input_dir=''):
     lines = []
     for key, (value, status) in diff.items():
         dir = '.'.join((input_dir, key)) if input_dir else key
-        dir_with_quotes = construct(dir)
-        line = None
+        dir_with_quotes = f"'{dir}'"
         match status:
             case 'removed':
-                line = MESSAGE_START + dir_with_quotes + ' was removed'
+                line = LINE_START + dir_with_quotes + ' was removed'
             case 'added':
-                line = (MESSAGE_START + dir_with_quotes
-                        + ' was added with value: '
-                        + construct(value))
-            case 'nested':
-                line = format_inner(value, dir)
+                line = (LINE_START + dir_with_quotes + ' was added with value: '
+                        + format_inner(value))
             case 'updated':
-                line = (MESSAGE_START + dir_with_quotes
-                        + ' was updated. From '
+                line = (LINE_START + dir_with_quotes + ' was updated. From '
                         + ' to '.join(
-                          [construct(sub_value)
-                           for sub_value in value])
-                        )
-        line = line + '\n' if line and status != 'nested' else line
+                            [format_inner(sub_value) for sub_value in value]
+                        ))
+            case 'nested':
+                line = format(value, dir)
+            case 'same':
+                line = None
         lines.append(line) if line else None
-    return ''.join(lines)
+    return '\n'.join(lines)
 
 
-def construct(value):
-    match str(type(value))[8:-2]:
-        case 'bool':
-            return str(value).lower()
-        case 'NoneType':
-            return 'null'
-        case 'str':
-            return f"'{value}'"
-        case 'dict':
-            return '[complex value]'
-        case _:
-            return str(value)
+def format_inner(inner):
+    if isinstance(inner, bool):
+        return str(inner).lower()
+    elif inner is None:
+        return 'null'
+    elif isinstance(inner, str):
+        return f"'{inner}'"
+    elif isinstance(inner, dict):
+        return '[complex value]'
+    else:
+        return str(inner)
